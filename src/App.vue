c4669bd313a0f5c4440bcccb6320eb07efa20951
FilipeBento19 <filipepvp1903@gmail.com>
2025-10-14 08:55:03 -0300

    commit initial

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/filmes">Filmes</router-link>
+      <router-link to="/tv">Programas de TV</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
+</template>
+
+<style scoped>
+header {
+  height: 3rem;
+  display: flex;
+  background-color: black;
+  color: #fff;
+  font-size: 1.2rem;
+  padding-left: 2rem;
+}
+
+nav {
+  column-gap: 2rem;
+  margin-bottom: 0;
+  display: flex;
+  align-items: center;
+}
+
+nav a {
+  text-decoration: none;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border-radius: 0.25rem;
+  transition: background-color 0.3s;
+}
+
+nav a:hover {
+  background-color: #333;
+}
+
+nav a.router-link-active {
+  background-color: #387250;
+}
+
+main {
+  padding: 2rem;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+
+<template>
+  <div>
+    <h1>Bem-vindo ao TMDB Explorer! 🎬</h1>
+    <p>Use o menu acima para navegar entre Filmes e Programas de TV.</p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  padding: 2rem;
+  text-align: center;
+}
+
+h1 {
+  margin-bottom: 1rem;
+}
+</style>
